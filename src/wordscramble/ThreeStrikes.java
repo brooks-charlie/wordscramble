ffb63965391fa019c84a42ce6aaa515515da4c49
SeaBronson <nob10003@byui.edu>
2014-05-10 20:59:20 -0500

    PlayAgain class will be further worked on as more is learned throughout the course which would contain If statements or a more efficient way added to display

--- src/wordscramble/ThreeStrikes.java
+++ src/wordscramble/ThreeStrikes.java
@@ -11,5 +11,9 @@ package wordscramble;
  * @author Sara
  */
 public class ThreeStrikes {
-    
+ //perimeters of the InputBox because the guesses are linked to that class. 
+ public String first = "X"; //vision of the first strike. Will try to get it to appear like that
+ public String second = "XX"; //vision of the second stroke. Again will try to have it appear when i learn it 
+ public String third = "XXX"; //vision of the third strike, i currently have no idea how to show it, just writing the variables to set it up.       
+   //will add more as we learn more.
 }
