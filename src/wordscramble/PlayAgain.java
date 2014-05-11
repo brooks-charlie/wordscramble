ab9f1cdd5a1955849c9ab45a589ad5d1c6cfaae4
SeaBronson <nob10003@byui.edu>
2014-05-10 20:49:02 -0500

    PlayAgain class will be further worked on as more is learned throughout the course which would contain If statements or a more efficient way

---
+++ src/wordscramble/PlayAgain.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package wordscramble;
+
+
+/**
+ *
+ * @author Sara
+ */
+public class PlayAgain{
+       public void display() {
+        System.out.println("Would you like to play again?");    
+            String yes ="true";
+            String no ="false";
+               /* if(true) 
+                    Return new Word Scramble(); 
+                    // returns the user to the menu to start the game. 
+                if(false) // will be adjusted later 
+                    System.out.println("Thank you for playing!");
+                 */   
+    }
+}
