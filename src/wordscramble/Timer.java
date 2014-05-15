3d511278bdc03030059338091301538d5f939a06
SeaBronson <nob10003@byui.edu>
2014-05-15 13:21:25 -0500

    PlayAgain class will be further worked on as more is learned throughout the course which would contain If statements or a more efficient way added to display

--- src/wordscramble/Timer.java
+++ src/wordscramble/Timer.java
@@ -10,12 +10,43 @@ package wordscramble;
  *
  * @author Sara
  */
-public class Timer {
-   public void display(){
-   String start= "5";
-   String finsih = "8";
-   String time = "finish - start";
-    System.out.println("the word took" + time + "seconds to solve!");  
-      
-}
-}
+public class Timer { 
+   private long startTime=0;
+   private long stopTime=0;
+   private boolean running=false;
+   public void start(){
+   this.startTime=System.currentTimeMillis();
+   this.running=true;
+   }
+   public void stop(){
+   this.stopTime=System.currentTimeMillis();
+   this.running=false;
+   }
+    //elaspsed time in milliseconds
+    public long getElapsedTime() {
+        long elapsed;
+        if (running) {
+             elapsed = (System.currentTimeMillis() - startTime);
+        }
+        else {
+            elapsed = (stopTime - startTime);
+        }
+        return elapsed;
+    }
+    
+    
+    //converting to seconds
+    public long getElapsedTimeSecs() {
+        long elapsed;
+        if (running) {
+            elapsed = ((System.currentTimeMillis() - startTime) / 1000);
+        }
+        else {
+            elapsed = ((stopTime - startTime) / 1000);
+        }
+        return elapsed;
+    }
+   
+  
+}   
+
