3723cffffa15b22da38260b35ab3994c680de83b
Raby-Aubrey <rab12001@byui.edu>
2014-05-21 18:35:18 -0800

    Raby - Third Individual assignment, added MainMenuView and MainMenuControl classes

---
+++ src/wordscramble/MainMenuControl.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package wordscramble;
+
+/**
+ *
+ * @author Aubrey
+ */
+public class MainMenuControl {
+    
+    public void displayWordLengthMenu() {
+        WordLengthMenuView wordLengthMenu = new WordLengthMenuView();
+        wordLengthMenu.getInput();
+    }
+    
+    public void displayPlayModeMenu() {
+        PlayModeMenuView playMenu = new PlayModeMenuView();
+        playMenu.getInput();
+    }
+   
+    public void displayHelpMenu() {
+        HelpMenuView helpMenu = new HelpMenuView();
+        helpMenu.getInput();
+    }
+}
---
+++ src/wordscramble/MainMenuView.java
@@ -0,0 +1,76 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package wordscramble;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Aubrey
+ */
+public class MainMenuView {
+        
+    private static final String[][] menuItems = {
+        {"W", "Word Length Menu"},
+        {"P", "Play Mode Menu"},
+        {"H", "Help Menu"},
+        {"X", "Exit Word Scramble"}
+    }; 
+  
+    MainMenuControl mainMenuControl = new MainMenuControl();
+    
+    public MainMenuView() {
+
+    }
+ 
+    
+    public void getInput() {       
+
+        String command;
+        Scanner inFile = new Scanner(System.in);
+        
+        do {
+            this.display(); // display the menu
+
+            // get commaned entered
+            command = inFile.nextLine();
+            command = command.trim().toUpperCase();
+            
+            switch (command) {
+                case "W":
+                    this.mainMenuControl.displayWordLengthMenu();
+                    break;
+                case "2":
+                    this.mainMenuControl.displayPlayModeMenu();
+                    break;
+                case "H":
+                    this.mainMenuControl.displayHelpMenu();            
+                    break;
+                case "X":
+                    break;
+                default: 
+                    new WordScrambleError().displayError("Invalid command. Please enter a valid command.");
+                    continue;                    
+            }
+        } while (!command.equals("X"));
+
+        return;
+    }
+    
+
+    
+    
+   public final void display() {
+        System.out.println("\n\t===============================================================");
+        System.out.println("\tEnter the letter associated with one of the following commands:");
+
+        for (int i = 0; i < MainMenuView.menuItems.length; i++) {
+            System.out.println("\t   " + menuItems[i][0] + "\t" + menuItems[i][1]);
+        }
+        System.out.println("\t===============================================================\n");
+    }
+}
