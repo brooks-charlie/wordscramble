c41a1acb786cf0f7bda60c55a62b2d5fe04eaa11
brooks-charlie <charlie_brooks1996@yahoo.com>
2014-06-28 18:11:30 -0600

    Charlie - Lesson 9 individual assignment - created score superclass

---
+++ src/wordscramble/ScoreCalc.java
@@ -0,0 +1,69 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package wordscramble;
+
+import java.io.Serializable;
+
+/**
+ *
+ * @author Aubrey M. Raby
+ * 
+ * tightly cohesive, loosely coupled, serializable- Heather 6/19/14
+ */
+public class ScoreCalc extends Score implements Serializable  {
+
+    public ScoreCalc() {
+    }
+
+    @Override
+    public int displayScore(int wordLength, int failedAttempts) {
+ 
+        System.out.println("You played with " + wordLength + " letters and used " + failedAttempts + " tries.");
+        int numLetters = wordLength;
+        int numFailed = failedAttempts;
+        if (numFailed > 2) {
+            System.out.println("Invalid number of attempts");
+        }
+        if (numLetters < 3 || numLetters > 6) {
+            System.out.println("Invalid number of letters");
+        }
+
+        int totalScore = (numLetters - numFailed) * 100;
+
+        if (numFailed == 0) {
+            totalScore += 500;
+            System.out.println("You are the ultimate word \"de-scrambler\"");
+
+        } else {
+            System.out.println("Good job!\nPlay again!");
+        }
+        System.out.println("Your score is " + totalScore);
+        if (WordScramble.numGames == 0) {
+            ScoreKeeper myKeepScore = new ScoreKeeper();
+            myKeepScore.scoreFirstGame(totalScore);
+            WordScramble.numGames++;
+
+        } else {
+            ScoreKeeper myKeepScore = new ScoreKeeper();
+            myKeepScore.scoreGames(totalScore);
+            WordScramble.numGames++;
+        }
+        System.out.println("numGames played is: " + WordScramble.numGames);
+
+        return totalScore;
+    }
+
+    @Override
+    public int scoreFirstGame(int totalScore) {
+        return 0;
+           }
+
+    @Override
+    public int scoreGames(int totalScore) {
+        return 0;
+          }
+
+   }
---
+++ src/wordscramble/ScoreKeeper.java
@@ -0,0 +1,53 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package wordscramble;
+
+import java.util.Arrays;
+
+/**
+ *
+ * @author charliebrooks
+ * * tightly cohesive and loosely coupled 6/18/2014
+ */
+public class ScoreKeeper extends Score {
+    
+    int[] tempScores = new int[WordScramble.numGames + 2];
+
+    public ScoreKeeper() {
+        //super(ScoreKeeper.tempScores);
+    }
+    
+   
+@Override
+    public int scoreFirstGame(int totalScore) { 
+        WordScramble.scores[WordScramble.numGames] = totalScore; 
+        return WordScramble.scores[WordScramble.numGames];
+    }
+@Override
+    public int scoreGames(int totalScore) { 
+
+        WordScramble.scores[WordScramble.numGames] = totalScore;
+
+        int[] tempScores = new int[WordScramble.numGames + 2];
+        for (int i = 0; i < WordScramble.scores.length; i++) {
+            tempScores[i] = WordScramble.scores[i];
+        }
+
+        WordScramble.scores = new int[WordScramble.numGames + 2];
+
+        for (int i = 0; i < WordScramble.scores.length; i++) {
+            WordScramble.scores[i] = tempScores[i];
+        }
+
+        return totalScore;
+    }
+
+    @Override
+    public int displayScore(int wordLength, int failedAttempts) {
+       return 0;
+    }
+
+}
