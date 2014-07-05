81022881fddb2e106767bb2c49bb192b049544e9
brooks-charlie <charlie_brooks1996@yahoo.com>
2014-07-05 10:39:28 -0600

    Charlie - new score package

--- src/citbyui/cit260/wordscramble/score/ScoreKeeper.java
+++ src/citbyui/cit260/wordscramble/score/ScoreKeeper.java
@@ -3,9 +3,10 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package wordscramble;
+package citbyui.cit260.wordscramble.score;
 
 import java.util.Arrays;
+import wordscramble.WordScramble;
 
 /**
  *
