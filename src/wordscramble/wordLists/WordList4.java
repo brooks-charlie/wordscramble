6f5779a5c9f290d2bf55265ab5888d586e5d2f1b
SeaBronson <nob10003@byui.edu>
2014-07-05 19:30:46 -0500

    Sara-WordLists package created

---
+++ src/wordscramble/wordLists/WordList4.java
@@ -0,0 +1,88 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package wordscramble.wordLists;
+
+import java.io.Serializable;
+import java.util.Arrays;
+import java.util.Random;
+import java.util.Scanner;
+
+/**
+ *
+ * @author charliebrooks
+ * 
+ *
+*/
+public class WordList4 implements Serializable {
+    private String wordLength = "4";
+    private String wordToScramble;// = "cats";
+    private String[] wordListArray4 = {
+        "dogs", "cats", "tape", "hats", "robs",
+        "cars", "main", "hams", "bone", "tame", 
+        "tall", "mane", "rain", "ruin", "seat"
+    };
+    
+    private double randomNum;
+    private int playerWordLength;
+    private int useRandomNum;
+
+    public WordList4() {
+    }
+
+    public String getWordLength() {
+        return wordLength;
+    }
+
+    public void setWordLength(String wordLength) {
+        this.wordLength = wordLength;
+    }
+
+    public String getWordToScramble() {
+        return wordToScramble;
+    }
+
+    public void setWordToScramble(String wordToScramble) {
+        this.wordToScramble = wordToScramble;
+    }
+
+    public String[] getWordListArray() {
+        return wordListArray4;
+    }
+
+    public void setWordListArray(String[] wordListArray) {
+        this.wordListArray4 = wordListArray;
+    }
+    /* This function will pick a random word from the wordList array
+    public void chooseWord(){
+     wordToScramble = (wordListArray[new Random().nextInt(wordListArray.length)]);
+    }
+    */
+    public String chooseWord(int wordLength) {
+
+       // playerWordLength = Integer.parseInt(wordLength);
+    
+        randomNum = new Random().nextInt(wordListArray4.length);
+
+        if (randomNum >= wordListArray4.length) {
+
+            randomNum = wordListArray4.length - 1;
+        }
+        useRandomNum = (int) randomNum;
+        wordToScramble = (wordListArray4[useRandomNum]);
+
+        System.out.println("This is the Word to scramble:\t\"" + wordToScramble + "\"");
+        return wordToScramble;
+    }
+    
+    public void displayVariables(){
+         System.out.println("This is the number of letters in the scrambled word: " + this.wordLength + "\n");
+         System.out.println("This is the list of words: \n" + Arrays.toString(this.wordListArray4));
+         System.out.println("This is the word that will be scrambled: " + this.wordToScramble);
+     }
+    
+    
+}
---
+++ src/wordscramble/wordLists/WordList5.java
@@ -0,0 +1,84 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package wordscramble.wordLists;
+
+import java.io.Serializable;
+import java.util.Arrays;
+import java.util.Random;
+
+/*
+ *
+ * @author charliebrooks
+ */
+public class WordList5 implements Serializable {
+    private String wordLength = "5";
+    private String wordToScramble;// = "canes";
+    private String[] wordListArray5 = {
+        "drain", "canes", "traps", "hates", "rumba",
+        "cares", "mixer", "hands", "boxes", "tones", 
+        "tares", "mines", "rains", "ruins", "saint"
+    };
+    private double randomNum;
+    private int playerWordLength;
+    private int useRandomNum;
+
+    public WordList5() {
+    }
+
+    public String getWordLength() {
+        return wordLength;
+    }
+
+    public void setWordLength(String wordLength) {
+        this.wordLength = wordLength;
+    }
+
+    public String getWordToScramble() {
+        return wordToScramble;
+    }
+
+    public void setWordToScramble(String wordToScramble) {
+        this.wordToScramble = wordToScramble;
+    }
+
+    public String[] getWordListArray() {
+        return wordListArray5;
+    }
+
+    public void setWordListArray(String[] wordListArray) {
+        this.wordListArray5 = wordListArray;
+    }
+    /* This function will pick a random word from the wordList array
+    public void chooseWord(){
+     wordToScramble = (wordListArray[new Random().nextInt(wordListArray.length)]);
+    }
+    */
+    public String chooseWord(int wordLength) {
+
+        //playerWordLength = Integer.parseInt(wordLength);
+    
+        randomNum = new Random().nextInt(wordListArray5.length);
+
+        if (randomNum >= wordListArray5.length) {
+
+            randomNum = wordListArray5.length - 1;
+        }
+        useRandomNum = (int) randomNum;
+        wordToScramble = (wordListArray5[useRandomNum]);
+
+        System.out.println("This is the Word to scramble:\t\"" + wordToScramble + "\"");
+        return wordToScramble;
+    }
+    
+    public void displayVariables(){
+         System.out.println("This is the number of letters in the scrambled word: " + this.wordLength + "\n");
+         System.out.println("This is the list of words: \n" + Arrays.toString(this.wordListArray5));
+         System.out.println("This is the word that will be scrambled: " + this.wordToScramble);
+     }
+    
+    
+}
