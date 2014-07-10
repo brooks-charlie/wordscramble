050682d58baa076b4c2d985e6823c8331ab5264e
Raby-Aubrey <rab12001@byui.edu>
2014-07-09 19:58:59 -0800

    Raby - individual week 11 assignment again

--- src/citbyui/cit260/wordscramble/menu/views/WordMenuView.java
+++ src/citbyui/cit260/wordscramble/menu/views/WordMenuView.java
@@ -106,6 +106,9 @@ public class WordMenuView {
            throw new MenuException();
            
         }
+       else{
+       this.wordMenuControl.wordlength(numLetters);
+       }
        
    }
 }
