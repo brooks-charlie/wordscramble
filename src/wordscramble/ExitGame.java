bf36db2c8c45bfcc6205c56192b4d11836386c09
brooks-charlie <charlie_brooks1996@yahoo.com>
2014-07-12 01:47:17 -0600

    Charlie - added exitGame class

--- src/wordscramble/ExitGame.java
+++ src/wordscramble/ExitGame.java
@@ -3,25 +3,44 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package wordscramble;
 
+import citbyui.cit260.wordscramble.menu.views.MainMenuView;
 import citbyui.cit260.wordscramble.stats.StatsAvg;
 import citbyui.cit260.wordscramble.stats.StatsMax;
+import java.util.Scanner;
 
 /**
  *
  * @author charliebrooks
  */
 public class ExitGame {
-    public void exitGame(){
-           TrackGames myTrackGames = new TrackGames();
-                    StatsMax myStatsMax = new StatsMax();
-                    myStatsMax.CalcStats();
-                    StatsAvg myStatsAvg = new StatsAvg();
-                    myStatsAvg.CalcStats();
-                    System.out.println("\tGames played:\t"+WordScramble.numGames+"\n\tGames won:\t"+myTrackGames.totalWins());
-                    System.exit(0);
+
+    private static final Scanner inFile = new Scanner(System.in);
+    private String response;
+
+    public void exitGame() {
+
+        System.out.println("Here are your stats so far:\n");
+        TrackGames myTrackGames = new TrackGames();
+        StatsMax myStatsMax = new StatsMax();
+        myStatsMax.CalcStats();
+        StatsAvg myStatsAvg = new StatsAvg();
+        myStatsAvg.CalcStats();
+
+        System.out.println("\tGames played:\t" + WordScramble.numGames + "\n\tGames won:\t" + myTrackGames.totalWins());
+        System.out.println("\nAre you sure you want to exit?");
+
+        System.out.println("Please answer Y or N.");
+        response = inFile.next();
+        response = response.trim().toUpperCase();
+        if ("Y".equals(response)) {
+            System.exit(0);
+        } else {
+            MainMenuView myMainMenu = new MainMenuView();
+            myMainMenu.getInput();
+        }
+
     }
-    
+
 }
